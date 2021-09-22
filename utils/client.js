2a012fef3600fffeac9bec263b49448a5ef4b807
= <=>
2021-09-22 15:39:54 +0700

    fix spelling error

--- utils/client.js
+++ utils/client.js
@@ -3,6 +3,6 @@ export function classNames(...classes) {
 }
 
 export const BASE_URL =
-  process.env.NODE_ENV !== "productions"
+  process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
     : "https://books-graphql-api.vercel.app";
